2a0d0f1fe9628fdc8e67a79535f9b541ab99cc5a
Videnoff <ivan.videnov@hotmail.com>
2021-05-31 23:23:31 +0300

    Fix views

---
+++ Web/BarakaBg.Web.ViewModels/Products/IngredientsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BarakaBg.Web.ViewModels.Products
+{
+    using BarakaBg.Data.Models;
+    using BarakaBg.Services.Mapping;
+
+    public class IngredientsViewModel : IMapFrom<ProductIngredient>
+    {
+        public string IngredientName { get; set; }
+    }
+}
---
+++ Web/BarakaBg.Web.ViewModels/Products/SingleProductViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace BarakaBg.Web.ViewModels.Products
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using AutoMapper;
+    using BarakaBg.Data.Models;
+    using BarakaBg.Services.Mapping;
+
+    public class SingleProductViewModel : IMapFrom<Product>, IHaveCustomMappings
+    {
+        public string Name { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public double Price { get; set; }
+
+        public string Description { get; set; }
+
+        public IEnumerable<IngredientsViewModel> Ingredients { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Product, ProductInListViewModel>()
+                .ForMember(x => x.ImageUrl, opt => opt.MapFrom(x => x.Images.FirstOrDefault().RemoteImageUrl != null
+                    ? x.Images.FirstOrDefault().RemoteImageUrl
+                    : "/images/products/" + x.Images.FirstOrDefault().Id + "." + x.Images.FirstOrDefault().Extension));
+        }
+    }
+}
