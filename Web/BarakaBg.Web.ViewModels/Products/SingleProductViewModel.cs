904cfb0728c6114d04b0e4af042a604e7c65cb0f
Videnoff <ivan.videnov@hotmail.com>
2021-06-03 22:57:17 +0300

    Fix average votes

--- Web/BarakaBg.Web.ViewModels/Products/SingleProductViewModel.cs
+++ Web/BarakaBg.Web.ViewModels/Products/SingleProductViewModel.cs
@@ -30,7 +30,9 @@
         public void CreateMappings(IProfileExpression configuration)
         {
             configuration.CreateMap<Product, SingleProductViewModel>()
-                .ForMember(x => x.AverageVote, opt => opt.MapFrom(x => x.Votes.Average(v => v.Value)))
+                .ForMember(
+                    x => x.AverageVote,
+                    opt => opt.MapFrom(x => x.Votes.Count == 0 ? 0 : x.Votes.Average(v => v.Value)))
                 .ForMember(x => x.ImageUrl, opt => opt.MapFrom(x => x.Images.FirstOrDefault().RemoteImageUrl != null
                     ? x.Images.FirstOrDefault().RemoteImageUrl
                     : "/images/products/" + x.Images.FirstOrDefault().Id + "." + x.Images.FirstOrDefault().Extension));
