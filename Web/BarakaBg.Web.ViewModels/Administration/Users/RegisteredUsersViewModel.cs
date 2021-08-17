c25d15487dbd8b89ff134259d901264a8a17cc51
Videnoff <ivan.videnov@hotmail.com>
2021-08-17 23:38:36 +0300

    Add Admin Theme

---
+++ Web/BarakaBg.Web.ViewModels/Administration/Users/RegisteredUsersViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BarakaBg.Web.ViewModels.Administration.Users
+{
+    public class RegisteredUsersViewModel
+    {
+        public string RegistrationDate { get; set; }
+
+        public int UsersCount { get; set; }
+    }
+}
---
+++ Web/BarakaBg.Web/Areas/Administration/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿namespace BarakaBg.Web.Areas.Administration.Controllers
+{
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    using BarakaBg.Data.Models;
+    using BarakaBg.Web.ViewModels.Administration.Users;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class UsersController : AdministrationController
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult RegisteredUsers()
+        {
+            var registeredUsers = new List<RegisteredUsersViewModel>();
+            var userDate = this.userManager.Users.OrderBy(x => x.CreatedOn).ToList().GroupBy(x => x.CreatedOn
+                .ToString("dd-MMM-yyy", CultureInfo.InvariantCulture));
+
+            var usersCount = 0;
+
+            foreach (var user in userDate)
+            {
+                usersCount += user.Count();
+                registeredUsers.Add(new RegisteredUsersViewModel
+                {
+                    RegistrationDate = user.Key,
+                    UsersCount = usersCount,
+                });
+            }
+
+            return this.Json(registeredUsers);
+        }
+    }
+}
